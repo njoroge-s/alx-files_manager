e9a08bf0e892d597fc78a47a4e155ef4d31686e4
njoroge-s <sarahnjoroge2015@gmail.com>
2023-05-28 12:11:21 +0300

    AppController

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-import { Express } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
@@ -6,8 +6,8 @@ import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-router.get('/status', getStatus);
-router.get('/stats', getStats);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
@@ -15,3 +15,4 @@ router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 
 export default router;
+
